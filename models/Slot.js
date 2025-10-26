0b8df44e5d9841b20e6f8af596929d5e358d08bf
Phan Văn Khải <phankhai1142003@gmail.com>
2025-10-27 02:49:52 +0700

    add fake data slot

--- models/Slot.js
+++ models/Slot.js
@@ -1,12 +1,21 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const slotSchema = new mongoose.Schema({
-    slotNumber: { type: Number, required: true },
-    name: { type: String, required: true },
-    startTime: { type: String, required: true },
-    endTime: { type: String, required: true },
-})
+  slotNumber: { type: Number, required: true },
+  name: { type: String, required: true },
+  startTime: { type: String, required: true },
+  endTime: { type: String, required: true },
+});
 
-const Slot = mongoose.model('Slot', slotSchema, "slots ");
+const Slot = mongoose.model("Slot", slotSchema, "slots ");
 
 module.exports = Slot;
+
+//fake data
+/*
+[
+    { "slotNumber": 1, "name": "Slot 1", "startTime": "07:30", "endTime": "09:50" },
+  { "slotNumber": 2, "name": "Slot 2", "startTime": "10:00", "endTime": "12:20" },
+  { "slotNumber": 3, "name": "Slot 3", "startTime": "12:50", "endTime": "15:10" },
+  { "slotNumber": 4, "name": "Slot 4", "startTime": "15:20", "endTime": "17:40" },
+] */
